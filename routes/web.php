cf358712ca6c21a260e279d2e2b547aedc85da2c
Hamid Achaou <achaou.hamid.solicode@gmail.com>
2025-07-25 23:22:49 +0100

    install breez autheentication

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
-use App\Jobs\ProcessPodcast;
-use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    ProcessPodcast::dispatch(1)->onqueue('hight');
-    ProcessPodcast::dispatch(2)->onqueue('low');
-    return "test route";
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
